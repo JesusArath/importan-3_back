db040970f713bf1b47a7f58cb11e3f6f9a0901c3
JesusArath <jesus19arath@hotmail.com>
2020-12-05 16:12:45 -0600

    Se agrego documento Word

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
+    //Recibimos la informacion obtenida en variables via put
+        // y las ingresamos dentro del documento
+    const body = req.body;
+
+    //Libreria del Documento 
+
+
+
+
   res.send('Hola Mundo!');
 });
 
